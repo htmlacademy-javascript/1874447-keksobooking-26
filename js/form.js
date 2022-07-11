10c67c2bf9dd6199e80244a72eed775bd59b8714
Наташа <tertyshnayanatasha@gmail.com>
2022-07-11 15:22:54 +0200

    Правда или действие (часть 1)

--- js/form.js
+++ js/form.js
@@ -2,6 +2,36 @@ const adForm = document.querySelector('.ad-form');
 const mapFilter = document.querySelector('.map__filters');
 const disabledField = document.querySelectorAll('select.map__filter, fieldset');
 
+const roomNumber = adForm.querySelector('#room_number');
+const capacity = adForm.querySelector('#capacity');
+const guestNumber = capacity.querySelectorAll('option');
+
+const NumberOfGuests = {
+  1: ['1'],
+  2: ['1', '2'],
+  3: ['1', '2', '3'],
+  100: ['0'],
+};
+
+const validateRoom = () => {
+  const roomValue = roomNumber.value;
+
+  guestNumber.forEach((guest) => {
+    const isDisabled = (NumberOfGuests[roomValue].indexOf(guest.value) === -1);
+    guest.selected = NumberOfGuests[roomValue][0] === guest.value;
+    guest.disabled = isDisabled;
+    guest.hidden = isDisabled;
+  });
+};
+
+validateRoom();
+
+const onRoomNumberChange = () => {
+  validateRoom();
+};
+
+roomNumber.addEventListener('change', onRoomNumberChange);
+
 const setDisabledState = () => {
   disabledField.forEach((item) => {
     item.disabled = !item.disabled;
@@ -16,3 +46,5 @@ const toggleInteractive = () => {
 };
 
 toggleInteractive();
+
+export{adForm};
